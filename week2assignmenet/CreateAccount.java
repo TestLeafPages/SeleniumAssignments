c2b39df57754a27ffebfdfde93ad0a1d645a04d5
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-04-24 01:24:21 -0700

    Add files via upload

---
+++ week2assignmenet/CreateAccount.java
@@ -0,0 +1,44 @@
+package week2assignmenet;
+
+
+public class CreateAccount {
+
+	public static void main(String[] args) {
+		/*
+		 * //Pseudo Code
+		 * 
+		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
+		 * 
+		 * 2. Enter UserName and Password Using Id Locator
+		 * 
+		 * 3. Click on Login Button using Class Locator
+		 * 
+		 * 4. Click on CRM/SFA Link
+		 * 
+		 * 5. Click on Accounts Button
+		 * 
+		 * 6. Click on Create Account
+		 *  
+		 * 7. Enter AccountName Field Using Xpath Locator value as Debit Limited Account
+		 * 
+		 * 8. Enter DEscriptiion as "Selenium Automation Tester"
+		 * 
+		 * 9. Enter LocalName Field Using Xpath Locator
+		 * 
+		 * 10. Enter SiteName Field Using Xpath Locator
+		 * 
+		 * 11. Enter value for AnnualRevenue Field using Xpath Locator but class as Attribute
+		 
+		 * 
+		 * 14. Click on Create Account using Xpath Locator
+                 * 15.Close the browser
+        
+      
+        
+      
+		 */
+
+
+	}
+
+}
---
+++ week2assignmenet/NTCreateAccount.java
@@ -0,0 +1,42 @@
+package week2assignmenet;
+
+public class NTCreateAccount {
+
+	public static void main(String[] args) {
+		/*
+		 * //Pseudo Code
+		 * 
+		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
+		 * 
+		 * 2. Enter UserName and Password Using Id Locator
+		 * 
+		 * 3. Click on Login Button using Class Locator
+		 * 
+		 * 4. Click on CRM/SFA Link
+		 * 
+		 * 5. Click on Accounts Button
+		 * 
+		 * 6. Click on Create Account
+		 * 
+		 * 7. Select preferred currency as Indian Rupee using SelectByValue
+		 * 
+		 * 8. Enter the Description text as "NRI Account"
+		 * 
+		 * 9. Enter LocalName Field Using Xpath Locator
+		 * 
+		 * 10. Enter SiteName Field Using Xpath Locator
+		 * 
+		 * 11. Enter value for AnnualRevenue Field using Xpath Locator but class as
+		  Attribute
+		 * 
+		
+		 * 
+		 * 17. Click on Create Account using Xpath Locator
+		 * 
+		 * 18. Get the Text of Error Message Displayed (hint: Red Texts)
+                 * close the browser
+		 */
+
+	}
+
+}
