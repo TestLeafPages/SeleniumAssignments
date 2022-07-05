b94b325def584744d58dff75acf77a3c228c96bf
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-07-05 13:33:13 +0530

    Add files via upload

---
+++ week1/day2/assignments/optional/Calculator.java
@@ -0,0 +1,26 @@
+package week1.day2;
+
+public class Calculator {
+public static void main(String[] args) {
+	/*
+	 * Create a class "Calculator" with below methods 
+	 * 
+	 * (a) add(int num1, int num2,int num3),
+	 *  it should return sum of num1+num2+num3 
+	 *  
+	 *  (b) sub(int num1, intnum2), 
+	 *  it should return subtraction of of num1-num2 
+	 *  
+	 *  (c) mul(double num1,double num2), 
+	 *  it should return product of num1 * num2 
+	 *  
+	 *  (d) divide(float num1,float num2), 
+	 *  it should return division of num1 / num2
+	 * 
+	 * 
+	 * -- Create another class as MyCalculator and call all the methods from
+	 * 
+	 * Calculator and print the results
+	 */
+}
+}
