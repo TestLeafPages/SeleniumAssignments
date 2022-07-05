b99fce851deb6826ec44291110860f3643e310f6
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-07-05 13:14:58 +0530

    Add files via upload

---
+++ week1/day1/assignments/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+public static void main(String[] args) {
+	/*Pseudocode
+	 * : ---------- 
+	 * 1. Initialize a number, say, int number= 35; 
+	 * 
+	 * 2. If a number is positive, print "The number is positive" 
+	 * 
+	 * If a number is negative, print "The number is negative" 
+	 * 
+	 * If it nether negative nor positive, print as
+	 * 
+	 * "The number is neither positive nor negative"
+	 */
+}
+}
---
+++ week1/day1/assignments/ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class ConvertANegativeNumberToPositiveNumber {
+public static void main(String[] args) {
+	/*
+	 * Pseudocode: 
+	 * 
+	 * 1. Initialize an integer with a negative number like,
+	 * int number = -40;
+	 * 
+	 * 2. Check if the number is a negative number 
+	 * 
+	 * 
+	 * Hint : any number that is lesser than zero is a negative number 
+	 * 
+	 * 
+	 * 3. If so, convert the number to a positive number
+	 * 
+	 * 
+	 * 4. Print the converted positive number
+	 */
+}
+}
