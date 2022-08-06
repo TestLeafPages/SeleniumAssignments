5acf43a670920ead7780717ba9c604228fbfda9a
shreeniryu <110728767+shreeniryu@users.noreply.github.com>
2022-08-06 19:49:05 +0530

    Update ConvertANegativeNumberToPositiveNumber.java
    Please review and let know your thoughts.

--- week1/day1/assignments/ConvertANegativeNumberToPositiveNumber.java
+++ week1/day1/assignments/ConvertANegativeNumberToPositiveNumber.java
@@ -1,24 +1,14 @@
 package week1.day1;
 
-public class ConvertANegativeNumberToPositiveNumber {
-public static void main(String[] args) {
-	/*
-	*Problem statement: Convert a negative number to positive number
-	 * Pseudocode: 
-	 * 
-	 * 1. Initialize an integer with a negative number like,
-	 * int number = -40;
-	 * 
-	 * 2. Check if the number is a negative number 
-	 * 
-	 * 
-	 * Hint : any number that is lesser than zero is a negative number 
-	 * 
-	 * 
-	 * 3. If so, convert the number to a positive number
-	 * 
-	 * 
-	 * 4. Print the converted positive number
-	 */
-}
+public class ConvertANegativeNumberToPositiveNumber
+{
+	public static void main(String[] args)
+	{
+		int number = -40;
+		if(number<0)
+		{
+			number = (~(positive - 1));
+			System.out.println("Converted positive number is = " +number);
+		}
+	}
 }
