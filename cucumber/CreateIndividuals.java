e902d5a0529468193c5c0c3c6f34c3721178c40e
TestLeafPages <earth@testleaf.com>
2023-01-29 14:47:45 +0530

    Update CreateIndividuals.java

--- cucumber/CreateIndividuals.java
+++ cucumber/CreateIndividuals.java
@@ -7,10 +7,9 @@ public class CreateIndividuals {
 	 *  1. Login to https://login.salesforce.com 
 	 *  2.Click on the toggle menu button from the left corner 
 	 *  3. Click View All and click Individuals from App Launcher
-	 *  4. Click on the Dropdown icon in the Individuals tab
-	 *  5. Click on New Individual 
-	 *  6. Enter the Last Name as 'Kumar'
-	 *  7.Click save and verify Individuals Name"
+	 *  4. Click on New Individual 
+	 *  5. Enter the Last Name as 'Kumar'
+	 *  6.Click save and verify Individuals Name"
 	 * 
 	 * 
 	 * Refer Step Video:
